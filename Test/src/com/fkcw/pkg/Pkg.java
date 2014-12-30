bff78ee434bafd74acf5fbbe94b8558449d51da7
fkcw <peter.zhang@fkcw.net>
2014-12-30 13:21:08 +0800

    NewPkg

---
+++ Test/src/com/fkcw/pkg/Pkg.java
@@ -0,0 +1,15 @@
+package com.fkcw.pkg;
+class Newpkg{
+	
+}
+public class Pkg {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
