e32838e25e085d9434b99e2d4ae51b73e2f62c85
fkcw <peter.zhang@fkcw.net>
2014-12-30 14:30:20 +0800

    Add 1st Swingmain.java

---
+++ Swing/src/com/fkcw/swing_main/Swingmain.java
@@ -0,0 +1,15 @@
+package com.fkcw.swing_main;
+
+import java.awt.*;
+
+public class Swingmain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
