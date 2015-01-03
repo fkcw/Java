811b1580c396c8f7da05c27884e4e07f0610edf0
peterzhang <peter.zhang@fkcw.net>
2015-01-03 00:05:42 -0800

    Revert "Wind example from Web"
    This reverts commit ce97c46d532c1d3844c42e021dfa50292d812784.

--- Swing/src/com/fkcw/swing_main/Swingmain.java
+++ Swing/src/com/fkcw/swing_main/Swingmain.java
@@ -3,38 +3,40 @@ package com.fkcw.swing_main;
 import java.awt.*;
 import javax.swing.*;
 
-public class Swingmain extends JFrame {
+public class Swingmain extends JFrame{
 
-	public Swingmain() {
+	/**
+	 * @param args
+	 */
+	public Swingmain(){
 		this.setTitle("Login Window");
-		this.setSize(1000,900);
-		JLabel jlname = new JLabel("Account:");
-		JLabel jlpass = new JLabel("Password:");
+		this.setSize(500, 400);
+		JLabel jlname = new JLabel("U:");
+		JLabel jlpass = new JLabel("P:");
 		JTextField jtname = new JTextField();
 		JTextField jtpass = new JTextField();
-		JButton jbok = new JButton("OK");
-		JButton jbcal = new JButton("Cancel");
+		JButton jbok = new JButton("l");
+		JButton jbcal = new JButton("c");
 		jlname.setBounds(60, 40, 180, 20);
 		jlpass.setBounds(60, 70, 180, 20);
-		jtname.setBounds(120, 40, 90, 20);
-		jtname.setBounds(120, 70, 90, 20);
-		jbok.setBounds(70, 110, 60, 20);
-		jbcal.setBounds(70, 130, 60, 20);
+		jtname.setBounds(120,40,90,20);
+		jtname.setBounds(120,70,90,20);
+		jbok. setBounds(70,110,60,20);
+		jbcal.setBounds(140,110,60,20);
 		Container cp = this.getContentPane();
-		cp.setBackground(getBackground());
 		cp.add(jlname);
 		cp.add(jlpass);
 		cp.add(jtname);
 		cp.add(jtpass);
-		cp.add(jbcal);
 		cp.add(jbok);
+		cp.add(jbcal);
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Swingmain login = new Swingmain();
+		login.setVisible(true);
 		
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Swingmain sw =new Swingmain();
-		sw.setVisible(true);
-		
-	}
 }
