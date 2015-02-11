573e1bada7ee2f03e7db6e9c3c2bd6d4b5c380b6
fkcw <peter.zhang@fkcw.net>
2015-02-11 20:36:16 +0800

    Color

--- CoBridge/src/com/fkcw/DBUtil/TBColor.java
+++ CoBridge/src/com/fkcw/DBUtil/TBColor.java
@@ -4,31 +4,36 @@ import java.awt.Color;
 import java.awt.Component;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableCellRenderer;
+import javax.tools.JavaCompiler;
 
 public class TBColor extends DefaultTableCellRenderer{
 	/**
 	 * 
 	 */
-	private String v1;
-	private String v2;
-	private static final DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 	private static Date date = new Date();
 	private static final long serialVersionUID = 1L;
+
 	DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
 	public Component getTableCellRendererComponent(JTable table, Object value,  
 			boolean isSelected, boolean hasFocus, int row, int column){
-		
-		if(value.toString()==(dateFormat.format(date)).toString()){
-			//System.out.println(dateFormat.format(date));
+//		returned = table.getValueAt(row, column);
+		if((value instanceof Date)&&(dateFormat.format(value).equals(dateFormat.format(date)))){
+			return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			
+		}else{
+			if(dateFormat.format(value).equals(dateFormat.format(date))){
 			dtcr.setBackground(Color.green);
 			return dtcr.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
-		}else{
-			
+			}else{
 			return super.getTableCellRendererComponent(table, value, isSelected, hasFocus, row, column);
+			}
 		}
 	}
 }
